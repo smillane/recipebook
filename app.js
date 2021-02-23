076c8c771657d3ab14c8e5efdff7187d6f9c2241
Sean Millane <jmillaneiv@gmail.com>
2021-02-23 17:48:54 -0500

    finish edit functionality, add delete functionality

--- app.js
+++ app.js
@@ -24,6 +24,20 @@ const app = express()
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
+
+// Method override
+app.use(
+    methodOverride(function (req, res) {
+        if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+            // look in urlencoded POST bodies and delete it
+            let method = req.body._method
+            delete req.body._method
+            return method
+        }
+    })
+)
+
+
 // logging
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
--- routes/recipes.js
+++ routes/recipes.js
@@ -70,5 +70,42 @@ router.get('/edit/:id', ensureAuth, async (req, res) => {
     }
 })
 
+// update recipe
+// PUT /recipes/:id
+
+router.put('/:id', ensureAuth, async (req, res) => {
+    let recipe = await Recipe.findById(req.params.id).lean()
+
+    if (!recipe) {
+        return res.render('error/404')
+    }
+
+    if (recipe.user != req.user.id) {
+        res.redirect('/recipes')
+    } else {
+        recipe = await Recipe.findOneAndUpdate({ _id: req.params.id }, req.body, {
+            new: true,
+            runValidators: true,
+        })
+
+        res.redirect('/dashboard')
+    }
+})
+
+// delete recipe
+// DELETE /recipes/:id
+
+router.delete('/:id', ensureAuth, async (req, res) => {
+    try {
+        await Recipe.remove({ _id: req.params.id })
+        res.redirect('/dashboard')
+    }catch(err) {
+        console.error(err)
+        return res.render('error/500')
+    }
+})
+
+
+
 
 module.exports = router
